a4ae600830b4b61f5c6857f81fc3f0b02bcc73d7
athilasb <athila-sb@hotmail.com.br>
2023-04-25 22:56:38 -0300

    Initial commit

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<?php echo("athila") ?> 
-<?php echo("athila") ?> 
-<?php echo("athila") ?> 
-=======
-<?php echo("athila") ?> 
->>>>>>> 47f70fda7cb2943371144fbae2afe241ac31a93b
+
+<?php echo "athila" ;?> 
+
